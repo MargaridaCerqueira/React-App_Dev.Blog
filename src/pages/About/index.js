d1471e0e555608acc90370c8db0d6f5d84e8e1bc
MargaridaCerqueira <79659696+MargaridaCerqueira@users.noreply.github.com>
2022-10-25 19:59:37 +0100

    Update 'about' page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,7 +1,7 @@
 function About() {
   return (
     <div className="about__container">
-      <h2 className="title_primary">About</h2>
+      <h2 className="title_primary">My First Project with React.JS</h2>
       <p className="about__content text">
         The idea was to create a blog build with React and using the Dev.to API
         where the user can search for a topic of their interest and all articles
